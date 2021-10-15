5e47f509375873232dc8fe55bf42b537054744e9
disha <disha_a@ee.iitr.ac.in>
2021-10-15 22:23:46 +0530

    react-quiz-master

---
+++ react-quiz-master/src/pages/Contact.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "../components/Header";
+import Profile from "../components/Profile";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+
+const Contactus = () => {
+  return (
+    <>
+      <Header />
+      <Profile/>
+
+      <Contact/>
+
+      <Footer />
+    </>
+  );
+};
+
+export default Contactus;
