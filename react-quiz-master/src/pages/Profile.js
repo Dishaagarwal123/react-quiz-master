4ef989217a64aee71742cf42618fd2ea2a6f1a69
disha <disha_a@ee.iitr.ac.in>
2021-10-19 10:20:39 +0530

    changes fourth time

--- react-quiz-master/src/pages/Profile.js
+++ react-quiz-master/src/pages/Profile.js
@@ -1,14 +1,15 @@
 import React from "react";
-import Header from "../components/Header";
-import Profile from "../components/Profile";
+
 import Contact from "../components/Contact";
+import Header from "../components/Header";
+
 import Footer from "../components/Footer";
 
-const Contactus = () => {
+const Myprofile = () => {
   return (
     <>
       <Header />
-      <Profile/>
+     
 
       <Contact/>
 
